015587c945f6703d83068ed9dd4c3c2b889eb755
mark-rodgers <11markrodgers11@gmail.com>
2017-08-25 16:21:08 -0700

    fixed duplicate activity bug, added debugging feature, cleaned up code

--- sparkpost.php
+++ sparkpost.php
@@ -53,7 +53,7 @@ function sparkpost_civicrm_enable() {
   $field_type  = CRM_Core_DAO::singleValueQuery("SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'civicrm_mailing_bounce_type' AND COLUMN_NAME = 'name'");
   $field_length  = CRM_Core_DAO::singleValueQuery("SELECT CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'civicrm_mailing_bounce_type' AND COLUMN_NAME = 'name'");
 
-  if($field_type != 'varchar' || $field_length < 24){
+  if($field_type != 'varchar' || $field_length < 24) {
     CRM_Core_DAO::singleValueQuery("ALTER TABLE civicrm_mailing_bounce_type CHANGE name name VARCHAR(24) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'Type of bounce'");
   }
 
@@ -66,6 +66,7 @@ function sparkpost_civicrm_enable() {
   sparkpost_addOptionValues();
   sparkpost_mailingsCheck();
 
+  Civi::settings()->set('com.pesc.sparkpost.debug', FALSE);
 }
 
 /**
@@ -144,36 +145,18 @@ function sparkpost_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
 }
 
 /**
- * Implementation of hook_civicrm_postEmailSend( )
- * Update the status of activity created in sparkpost_civicrm_alterMailParams
- */
-function sparkpost_civicrm_postEmailSend(&$params) {
-  // check if an activityId was added in hook_civicrm_alterMailParams
-  // if so, update the activity's status and add a target_contact_id
-
-  $temparr = $params;
-  unset($temparr['html']);
-  unset($temparr['text']);
-  $json = json_decode($temparr['headers']['X-MSYS-API']);
-  $activityID = $json->metadata->civi_activityid;
-
-  $result = civicrm_api3('Activity', 'create', array(
-    'sequential' => 1,
-    'id' => $activityID,
-    'status_id' => 'Completed'
-  ));
-  return TRUE;
-}
-
-/**
  * Implementation of hook_civicrm_alterMailParams( )
- * Inserts smtp headers for use in SparkPost API and subsequent bounce processing
+ * Inserts SMTP headers for use in SparkPost API and subsequent bounce processing
  */
 function sparkpost_civicrm_alterMailParams(&$params, $context) {
+  sparkpost_debug('Hooked civicrm_alterMailParams');
+  sparkpost_debug('params', $params);
+
   sparkpost_addOptionValues();
   sparkpost_mailingsCheck();
-  if($context == 'civimail'){ // Do this on bulk mail
 
+  // If this is a bulk email
+  if($context == 'civimail') {
     $temphash = explode('.',$params['Return-Path']);
     $hash = substr($temphash[3], 0, strpos($temphash[3], '@'));
 
@@ -182,7 +165,7 @@ function sparkpost_civicrm_alterMailParams(&$params, $context) {
       'hash' => $hash
     ));
 
-    //Prep SparkPost metadata
+    // Prepare SparkPost metadata
     $mailing = sparkpost_mailing($hash);
     $campaign = mb_convert_encoding($mailing['name'], 'UTF-8', 'auto');
 
@@ -199,83 +182,158 @@ function sparkpost_civicrm_alterMailParams(&$params, $context) {
         'civi_queue' => $mailEventQueue['values'][0]['id']
       )
     );
-  }else{ // Do this on transactional emails
+  }
 
-    // GET source contact ID for activity
-    $session = CRM_Core_Session::singleton();
-    $sourceContactID = $session->get('userID');
-    if (!$sourceContactID) {
-      $sourceContactID = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_Domain', CRM_Core_Config::domainID(), 'contact_id');
-    }
+  // If this is a transactional email
+  else {
+    /**
+     * The if-statement below is a temporary solution, until a bug which causes
+     * the civicrm_alterMailParams hook to be ran twice is fixed.
 
-    // GET activity type ID
-    $activityType = civicrm_api3('OptionValue', 'get', array('sequential' => 1,'option_group_id' => 'activity_type','name' => 'Transactional Email'));
-    $activityTypeID = $activityType['values'][0]['value'];
+     * The first time the hook runs, $params['subject'], $params['text'], and
+     * $params['html'] is not set. We wait until it is set to execute our code.
+     */
+    if(isset($params['subject'])) {
+      // If this is an offline transaction, get initiator's contact ID
+      $session = CRM_Core_Session::singleton();
+      $sourceContactID = $session->get('userID');
+      sparkpost_debug('sourceContactID', $sourceContactID);
 
-    // GET activity status ID
-    $activityStatus = civicrm_api3('OptionValue', 'get', array('sequential' => 1,'option_group_id' => 'activity_status','name' => 'Pending'));
-    $activityStatusID = $activityStatus['values'][0]['value'];
+      // If this is an online transaction, get domain contact ID
+      if (!$sourceContactID)
+        $sourceContactID = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_Domain', CRM_Core_Config::domainID(), 'contact_id');
 
-    //Create activity
-    $contactID = sparkpost_getTargetContactId($params);
-    $activityParams = array( 
-      'source_contact_id' => $sourceContactID,
-      'activity_type_id' => $activityTypeID, 
-      'subject' => $params['subject'],
-      'activity_date_time' => date('YmdHis'),
-      'status_id' => $activityStatusID,
-      'priority_id' => 2,
-      'details' => CRM_Utils_Array::value('html', $params, $params['text']),
-      'target_contact_id' => $contactID[0]
-    );
-    $newActivity = civicrm_api3('activity', 'create', $activityParams);
+      // Get target contact ID
+      $targetContactID = sparkpost_getTargetContactId($params)[0];
 
-    //Get email ID
-    $emailID = NULL;
-    $contactEmails = civicrm_api3('Email', 'get', array( //get all emails that match for this contact
-      'sequential' => 1,
-      'contact_id' => $contactID[0],
-      'email' => $params['toEmail']
-    ));
-    if($contactEmails['count']==1){ //if only one result use that
-      $emailID = $contactEmails['values'][0]['id'];
-    }else{
-      foreach ($contactEmails['values'] as $emailRecord) {
-        if($emailRecord['is_primary']) $emailID = $emailRecord['id']; //elseif primary, use primary
-      }
-      if(empty($emailID)) $emailID = $contactEmails['values'][0]['id']; //ok... just use the first one
-    }
-    //Prep email queue vars
-    $mailingJob = civicrm_api3('MailingJob', 'get', array(
-      'sequential' => 1,
-      'job_type' => 'SparkPost Transactional Emails',
-      'options' => array('limit' => 1)
-    ));
-    //Crate mailing event queue
-    $queueParams = array(
-      'job_id' => $mailingJob['id'],
-      'contact_id' => $contactID[0],
-      'email_id' => $emailID
-    );
-    $eventQueue = CRM_Mailing_Event_BAO_Queue::create($queueParams);
-    //Prep SparkPost metadata
-    if(!empty($newActivity['id'])){
-      $tags = array(
-        'campaign_id' => 'Transactional Email',
-        'metadata' => array(
-          'civi_type' => 'transactional',
-          'civi_hash' => $eventQueue->hash,
-          'civi_jobid' => $mailingJob['id'],
-          'civi_queue' => $eventQueue->id,
-          'civi_activityid' => $newActivity['id']
-        )
+      // Get activity type ID "Transactional Email"
+      $activityType = civicrm_api3('OptionValue', 'get', array('sequential' => 1,'option_group_id' => 'activity_type','name' => 'Transactional Email'));
+      $activityTypeID = $activityType['values'][0]['value'];
+      sparkpost_debug('activityType', $activityType);
+
+      // Get activity status ID for "Pending"
+      $activityStatus = civicrm_api3('OptionValue', 'get', array('sequential' => 1,'option_group_id' => 'activity_status','name' => 'Pending'));
+      $activityStatusID = $activityStatus['values'][0]['value'];
+      sparkpost_debug('activityStatus', $activityStatus);
+
+      // Create activity
+      $activityParams = array(
+        'source_record_id' => $sourceContactID,
+        'activity_type_id' => $activityTypeID,
+        'subject' => $params['subject'], // empty on contrib?
+        'activity_date_time' => date('YmdHis'),
+        'status_id' => $activityStatusID,
+        'priority_id' => 2,
+        'details' => CRM_Utils_Array::value('html', $params, $params['text']), // empty on contrib?
+        'target_contact_id' => $targetContactID
       );
+      sparkpost_debug('activityParams', $activityParams);
+
+      $newActivity = civicrm_api3('activity', 'create', $activityParams);
+      sparkpost_debug('newActivity', $newActivity);
+
+      // Get email ID
+      $emailID = NULL;
+      $contactEmails = civicrm_api3('Email', 'get', array(
+        'sequential' => 1,
+        'contact_id' => $targetContactID,
+        'email' => $params['toEmail']
+      ));
+      sparkpost_debug('contactEmails', $contactEmails);
+
+      /**
+       * If contact only has one matching email record, use that.
+
+       * If the contact has multiple matching email records (duplicates),
+       * and one of the duplicates is marked as primary, use that email.
+       * Otherwise, just use the first matching email that is returned.
+       */
+      if($contactEmails['count'] == 1) {
+        $emailID = $contactEmails['values'][0]['id'];
+      }
+      else {
+        foreach ($contactEmails['values'] as $emailRecord) {
+          if($emailRecord['is_primary'])
+            $emailID = $emailRecord['id'];
+        }
+        if(empty($emailID))
+          $emailID = $contactEmails['values'][0]['id'];
+      }
+      sparkpost_debug('emailID', $emailID);
+
+      // Get ID of 'SparkPost Transactional Email' mailing
+      $mailingJob = civicrm_api3('MailingJob', 'get', array(
+        'sequential' => 1,
+        'job_type' => 'SparkPost Transactional Emails',
+        'options' => array('limit' => 1)
+      ));
+      sparkpost_debug('mailingJob', $mailingJob);
+
+      // Create mailing event queue
+      $queueParams = array(
+        'job_id' => $mailingJob['id'],
+        'contact_id' => $targetContactID,
+        'email_id' => $emailID
+      );
+      sparkpost_debug('queueParams', $queueParams);
+
+      $eventQueue = CRM_Mailing_Event_BAO_Queue::create($queueParams);
+      sparkpost_debug('eventQueue', $eventQueue);
+
+      // Prepare SparkPost metadata
+      if(!empty($newActivity['id'])) {
+        $tags = array(
+          'campaign_id' => 'Transactional Email',
+          'metadata' => array(
+            'civi_type' => 'transactional',
+            'civi_hash' => $eventQueue->hash,
+            'civi_jobid' => $mailingJob['id'],
+            'civi_queue' => $eventQueue->id,
+            'civi_activityid' => $newActivity['id']
+          )
+        );
+      }
     }
   }
-  //Add SparkPost metadata to smtp headers
-  $json = json_encode($tags);
-  $params['headers']['X-MSYS-API'] = $json;
+  sparkpost_debug('tags', $tags);
 
+  // Add SparkPost metadata to SMTP headers
+  if(isset($tags)) {
+    $json = json_encode($tags);
+    $params['headers']['X-MSYS-API'] = $json;
+  }
+
+  sparkpost_debug('json', $json);
+  sparkpost_debug('params', $params);
+
+  sparkpost_debug('/Hooked civicrm_alterMailParams');
+  return TRUE;
+}
+
+/**
+ * Implementation of hook_civicrm_postEmailSend( )
+ * Update the status of activity created in sparkpost_civicrm_alterMailParams
+ */
+function sparkpost_civicrm_postEmailSend(&$params) {
+  sparkpost_debug('Hooked civicrm_postEmailSend');
+
+  $temparr = $params;
+  sparkpost_debug('temparr', $temparr);
+
+  $json = json_decode($temparr['headers']['X-MSYS-API']);
+  sparkpost_debug('json', $json);
+
+  $activityID = $json->metadata->civi_activityid;
+  sparkpost_debug('activityID', $activityID);
+
+  $result = civicrm_api3('Activity', 'create', array(
+    'sequential' => 1,
+    'id' => $activityID,
+    'status_id' => 'Completed'
+  ));
+  sparkpost_debug('result', $result);
+
+  sparkpost_debug('/Hooked civicrm_postEmailSend');
   return TRUE;
 }
 
@@ -389,7 +447,7 @@ function sparkpost_recentFetchSuccess() {
   catch (CiviCRM_API3_Exception $e) {
     $error = $e->getMessage();
   }
-  if(!empty($error)){
+  if(!empty($error)) {
       if (strpos($error, 'API (JobLog, get) does not exist') !== false) {
         return 0;
       }
@@ -402,7 +460,7 @@ function sparkpost_recentFetchSuccess() {
  * Perform CiviCRM API call to grab from addresses
  * @return string
  */
-function sparkpost_getFromAddresses(){
+function sparkpost_getFromAddresses() {
   $froms = '';
   $result = civicrm_api3('OptionValue', 'get', array(
     'sequential' => 1,
@@ -427,15 +485,15 @@ function sparkpost_getFromAddresses(){
 function sparkpost_getTargetContactId($params) {
   $emails = array_merge(
     explode(
-      ',', 
+      ',',
       $params['toEmail']
-    ), 
+    ),
     explode(
-      ',', 
+      ',',
       CRM_Utils_Array::value('cc', $params, '')
-    ), 
+    ),
     explode(
-      ',', 
+      ',',
       CRM_Utils_Array::value('bcc', $params, '')
     )
   );
@@ -445,7 +503,7 @@ function sparkpost_getTargetContactId($params) {
     if (!empty($matches[0])) {
       $targetContacts = sparkpost_getEmailContactId(trim($matches[0]));
       $targetContactIds = array_merge(
-        $targetContactIds, 
+        $targetContactIds,
         CRM_Utils_Array::value('contactIds', $targetContacts, array())
       );
     }
@@ -464,26 +522,26 @@ function sparkpost_getEmailContactId($email, $checkUnique = FALSE) {
       $email = $matches[0];
     }
   }
-  
+
   $emails['email'] = null;
-  $params = array( 
+  $params = array(
     'email' => $email,
     'return' => array('contact_id'),
     'api.Contact.get' => array('is_deleted' => 0, 'return' => array('id')),
   );
   $result = civicrm_api3('email', 'get', $params);
-  
-  //if contact not found then create new one
+
+  // If contact is not found, create a new one
   if (!$result['count']) {
     $contactParams = array(
       'contact_type' => 'Individual',
       'email' => $email,
     );
     civicrm_api3('contact', 'create', $contactParams);
-    $result = civicrm_api3('email', 'get', $params);      
+    $result = civicrm_api3('email', 'get', $params);
   }
-  
-  // changes done for bad data, sometimes there are multiple emails but without contact id   
+
+  // Changes done for bad data, sometimes there are multiple emails but without a contact id
   foreach ($result['values'] as $emailId => $emailValue) {
     if (CRM_Utils_Array::value('contact_id', $emailValue)
       && $emailValue['api.Contact.get']['count']
@@ -526,7 +584,7 @@ function sparkpost_updateBounceActivity($activityID,$reason) {
 }
 
 function sparkpost_addOptionValues() {
-  // create activity_type -> transactional email
+  // Create activity_type -> transactional email
   $activity_type_values = CRM_Core_OptionGroup::values('activity_type');
   if (!in_array('Transactional Email', $activity_type_values)) {
     $params = array(
@@ -538,10 +596,10 @@ function sparkpost_addOptionValues() {
     $activityObject = CRM_Core_OptionValue::addOptionValue($params, $groupParams, $action, $optionValueID);
   }
 
-  // create activity_status -> pending
+  // Create activity_status -> pending
   $activity_status_values = CRM_Core_OptionGroup::values('activity_status');
   if (!in_array('Pending', $activity_status_values)) {
-    //create activity_status value
+    // Create activity_status value
     $params = array(
       'label' => 'Pending',
       'is_active' => 1
@@ -551,10 +609,10 @@ function sparkpost_addOptionValues() {
     $activityObject = CRM_Core_OptionValue::addOptionValue($params, $groupParams, $action, $optionValueID);
   }
 
-  // create activity_status -> bounced
+  // Create activity_status -> bounced
   $activity_status_values = CRM_Core_OptionGroup::values('activity_status');
   if (!in_array('Bounced', $activity_status_values)) {
-    //create activity_status value
+    // Create activity_status value
     $params = array(
       'label' => 'Bounced',
       'is_active' => 1
@@ -565,13 +623,13 @@ function sparkpost_addOptionValues() {
   }
 }
 
-function sparkpost_mailingsCheck(){
-  //Add mailing - holds all transactional emails for bounce processing
-  //check for existing mailing 
+function sparkpost_mailingsCheck() {
+  // Add mailing - holds all transactional emails for bounce processing
+  // Check for existing mailing
   $mailingCheck = civicrm_api3('Mailing', 'get', array('name' => 'SparkPost Transactional Emails','domain_id' => CRM_Core_Config::domainID(),'options' => array('limit' => 1)));
 
-  //add mailing if it does not exist 
-  if($mailingCheck['count'] < 1){
+  // Add mailing if it does not exist
+  if($mailingCheck['count'] < 1) {
     $mailingName = $mailingJobType = 'SparkPost Transactional Emails';
     $mailingParams = array(
       'subject' => $mailingName,
@@ -582,15 +640,24 @@ function sparkpost_mailingsCheck(){
       'open_tracking' => FALSE,
       'is_completed' => TRUE,
     );
-    //create entry in civicrm_mailing
+    // Create entry in civicrm_mailing
     $mailing = CRM_Mailing_BAO_Mailing::add($mailingParams, CRM_Core_DAO::$_nullArray);
 
-    //create entry in civicrm_mailing_job
+    // Create entry in civicrm_mailing_job
     $mailingJob = new CRM_Mailing_DAO_MailingJob();
     $mailingJob->start_date = $mailingJob->end_date = date('YmdHis');
     $mailingJob->status = 'Complete';
     $mailingJob->job_type = $mailingJobType;
     $mailingJob->mailing_id = $mailing->id;
     $mailingJob->save();
+  }
+}
+
+function sparkpost_debug($k, $v = '__log') {
+  if(Civi::settings()->get('com.pesc.sparkpost.debug')) {
+    if($v == '__log')
+      CRM_Core_Error::debug_log_message('com.pesc.sparkpost: ' . $k);
+    else
+      CRM_Core_Error::debug_var('com.pesc.sparkpost.' . $k, $v, FALSE);
   }
 }
